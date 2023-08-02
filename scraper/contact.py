309ea2c893b560144844fdf6e8df16ddc37caca8
Esa112233 <57723779+Esa112233@users.noreply.github.com>
2023-08-02 02:10:01 +0000

    work on js decryption handle exceptions duplicates

--- scraper/contact.py
+++ scraper/contact.py
@@ -6,8 +6,17 @@ def at_check(words):
     words = words.split(' ')
     for i in words:
         if '@' in i:
+            print(i)
             return i
 
+def decodeEmail(e):
+    de = ""
+    k = int(e[:2], 16)
+
+    for i in range(2, len(e)-1, 2):
+        de += chr(int(e[i:i+2], 16)^k)
+
+    return de
 
 def email_search(url):
 
@@ -22,5 +31,5 @@ def email_search(url):
             print(email)
 
 
-if __name__ == "__main__":
-    email_search('https://www.10thstreetboxing.com/')
+#if __name__ == "__main__":
+   # email_search('https://www.accesswire.com/546295/Theramed-Provides-Update-on-New-Sales-Channel-for-Nevada-Facility')
--- scraper/scrape_boxing_canada.py
+++ scraper/scrape_boxing_canada.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import contact
 
 def get_google_search_links(query):
-    base_url = "https://www.google.com/search?q=boxing+club&oq=boxing&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGDkyDwgCEAAYQxiDARixAxiKBTIMCAMQABhDGLEDGIoFMgYIBBBFGDwyBggFEEUYPDIGCAYQRRg8MgYIBxBFGDzSAQgxMjg4ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8"
+    base_url = "https://www.google.com/search?q=motor+bike+leather+jackets&sxsrf=AB5stBijF4Tz0CHyEa1U1YyUnplWET0Wcw%3A1690940453998&ei=JbTJZNavPJKr0PEPnpyosA8&ved=0ahUKEwiW1uKI7LyAAxWSFTQIHR4OCvYQ4dUDCBA&uact=5&oq=motor+bike+leather+jackets&gs_lp=Egxnd3Mtd2l6LXNlcnAiGm1vdG9yIGJpa2UgbGVhdGhlciBqYWNrZXRzMgUQABiABDIIEAAYFhgeGAoyCxAAGBYYHhjxBBgKMggQABiKBRiGAzIIEAAYigUYhgMyCBAAGIoFGIYDSINVUJwYWMpScAp4AZABAZgB3QGgAfoUqgEGMjMuNS4xuAEDyAEA-AEBqAIUwgIHECMY6gIYJ8ICDRAuGMcBGNEDGOoCGCfCAhAQABiKBRjqAhi0AhhD2AEBwgIEECMYJ8ICBxAjGIoFGCfCAgcQABiKBRhDwgILEAAYgAQYsQMYgwHCAgsQLhiABBixAxiDAcICCxAAGIoFGLEDGIMBwgIKEC4YigUYsQMYQ8ICChAAGIoFGLEDGEPCAgUQLhiABMICDhAuGIAEGLEDGIMBGOUEwgIIEAAYgAQYsQPCAggQABiABBjJA8ICCBAAGIoFGJIDwgIIEAAYigUYsQPCAgcQABiABBgKwgIGEAAYFhgewgIIEAAYFhgeGA_CAgoQABgWGB4YDxgK4gMEGAAgQYgGAboGBggBEAEYAQ&sclient=gws-wiz-serp"
     params = {"q": query}
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
     res = requests.get(base_url, headers=headers)
@@ -24,5 +25,9 @@ for link in links:
     #print(link['href'])  # Access the 'href' attribute of each anchor tag
     if link['href'].startswith('https://'):
         link_list.append(link['href'])
+        
+        contact.email_search(link['href'])
 
-link_list =link_process(link_list)
+
+
+print(link_list)
